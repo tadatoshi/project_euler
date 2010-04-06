bf181c89d1f35827a2c0a48c13efe420dbcded86
Tadatoshi Takahashi <tadatoshi@tadatoshi-takahashis-computer.local>
2010-04-05 22:11:37 -0400

    Modified RDoc comment.

--- lib/project_euler/sums_of_digit_factorials.rb
+++ lib/project_euler/sums_of_digit_factorials.rb
@@ -1,7 +1,7 @@
 module ProjectEuler
   
   # == Problem 254: Sums of Digit Factorials
-  #  i.e. (from http://projecteuler.net/index.php?section=problems&id=254)
+  #  From http://projecteuler.net/index.php?section=problems&id=254:
   #    Define f(n) as the sum of the factorials of the digits of n. For example, f(342) = 3! + 4! + 2! = 32.
   #    Define sf(n) as the sum of the digits of f(n). So sf(342) = 3 + 2 = 5.
   #    Define g(i) to be the smallest positive integer n such that sf(n) = i. Though sf(342) is 5, sf(25) is also 5, and it can be verified that g(5) is 25.
